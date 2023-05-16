202ca25291c8b6c5ee22f1d3c17640f36712bebb
Michael Badocha <78705024+666callmesometimes@users.noreply.github.com>
2023-05-16 16:14:42 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+class App extends React.Component{
+
+    state = {
+        valuermb: null,
+        valuepln: null,
+        valueusd: null,
+        checkbox: sessionStorage.getItem('checkbox') === 'true' ? true : false
+
+    }
+
+    rmbConvert = (e) => {
+
+        this.setState({
+            valuermb: e.target.value,
+            valuepln: (e.target.value * 0.63).toFixed(2),
+            valueusd: (e.target.value * 0.15).toFixed(2),
+        })
+    }
+
+    plnConvert = (e) => {
+
+        this.setState({
+            valuermb: (e.target.value * 1.61).toFixed(2),
+            valuepln: e.target.value,
+            valueusd: (e.target.value * 0.23).toFixed(2),
+        })
+    }
+
+    usdConvert = (e) => {
+
+        this.setState({
+            valuermb: (e.target.value * 6.88).toFixed(2),
+            valuepln: (e.target.value * 4.29).toFixed(2),
+            valueusd: e.target.value,
+        })
+    }
+
+    handleClick = () => {
+        const checkbox = !this.state.checkbox;
+        this.setState({ checkbox });
+        sessionStorage.setItem('checkbox', checkbox);
+    }
+
+    componentDidMount() {
+        const checkbox = sessionStorage.getItem('checkbox') === 'true' ? true : false;
+        this.setState({ checkbox });
+      }
+
+    render(){
+
+        return(
+            <>
+            <div className={this.state.checkbox ? 'clicked' : ''}>
+            RMB: <input className={this.state.checkbox ? 'dark' : ''} type="number" id="rmb" placeholder="Insert value" value={this.state.valuermb} onChange={this.rmbConvert}/>¥
+            <br/>
+            PLN: <input className={this.state.checkbox ? 'dark' : ''} type="number" id="pln" placeholder="Insert value" value={this.state.valuepln} onChange={this.plnConvert} />zł
+            <br/>
+            USD: <input className={this.state.checkbox ? 'dark' : ''} type="number" id="usd" placeholder="Insert value" value={this.state.valueusd} onChange={this.usdConvert}/>$
+            <br/><br/>
+            <input type="checkbox" checked={this.state.checkbox} onClick={this.handleClick} className={'switch'}/>Darkmode/lightmode
+            </div>
+            </>
+        );
+    }
+}
+
+ReactDOM.render(<App/> , document.querySelector("#root"));
